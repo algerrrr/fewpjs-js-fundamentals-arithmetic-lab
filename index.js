310722e408682e7e0876c90292df7e79394afa0f
Algernon Soriano <alger.soriano@gmail.com>
2022-08-18 21:33:36 +1000

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 4 
+let num2 = 15.5
+let num3 = 11
+let num4 = 7
+
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 11) + 1
+
+let mod = num3 % num4
+
+let max = Math.max(5,20)
